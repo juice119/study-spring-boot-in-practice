bd3b623ceb368109044e46f0152a20cfbf6a6397
Juice <3221ju@gmail.com>
2023-10-12 06:43:28 +0900

    [STUDY-15] ch 2-1 PropertySource 를 통해서 환경 변수 추가

--- ch02/property-sources/src/main/java/com/manning/sbip/ch02/SpringBootAppDemoApplication.java
+++ ch02/property-sources/src/main/java/com/manning/sbip/ch02/SpringBootAppDemoApplication.java
@@ -1,13 +1,24 @@
 package com.manning.sbip.ch02;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootAppDemoApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(
+        SpringBootAppDemoApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootAppDemoApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(
+            SpringBootAppDemoApplication.class);
+
+        DbConfiguration dbConfiguration = applicationContext.getBean(DbConfiguration.class);
+        
+        log.info(dbConfiguration.toString());
     }
 
 }
