06224a0f465df6ca26d951d1f53b294b1f8cb714
Juice <3221ju@gmail.com>
2023-10-13 00:01:54 +0900

    [STUDY-15] 2.1.4 운영 체제 환경 변수 사용

--- ch02/os-env-variables/src/main/java/com/manning/sbip/ch02/SpringBootAppDemoApplication.java
+++ ch02/os-env-variables/src/main/java/com/manning/sbip/ch02/SpringBootAppDemoApplication.java
@@ -1,13 +1,25 @@
 package com.manning.sbip.ch02;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class SpringBootAppDemoApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(
+        SpringBootAppDemoApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootAppDemoApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(
+            SpringBootAppDemoApplication.class, args);
+        Environment env = applicationContext.getBean(Environment.class);
+        log.info(
+            "Configured application timeout value: " + env.getProperty(
+                "server.connection-timeout"));
     }
 
 }
