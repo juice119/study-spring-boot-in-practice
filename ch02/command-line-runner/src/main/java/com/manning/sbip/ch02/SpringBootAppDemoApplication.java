ec6804d7fa801bbc9ae2b4082b6c3848024e1d8c
Juice <3221ju@gmail.com>
2023-10-14 02:36:07 +0900

    [STUDY-15] 2.3 CommandLineRunner 인터페이스 및 Bean 을 통한 구현

--- ch02/command-line-runner/src/main/java/com/manning/sbip/ch02/SpringBootAppDemoApplication.java
+++ ch02/command-line-runner/src/main/java/com/manning/sbip/ch02/SpringBootAppDemoApplication.java
@@ -1,13 +1,34 @@
 package com.manning.sbip.ch02;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//public class SpringBootAppDemoApplication implements CommandLineRunner {
 public class SpringBootAppDemoApplication {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAppDemoApplication.class, args);
     }
 
+//    @Override
+//    public void run(String... args) throws Exception {
+//        // 스프링 부트 메일 클래스가 CommandLineRunner 인터페이스를 구현하여 애플리케이션 시작전 코드 수행
+//        logger.info("CourseTrackerApplication CommandLineRunner has executed");
+//    }
+    
+//    @Bean()
+//    public CommandLineRunner commandLineRunner() {
+//        return args -> {
+//            logger.info("CommandLineRunner executed as a bean definition with " + args.length
+//                + " arguments");
+//            for (int i = 0; i < args.length; i++) {
+//                logger.info("Argument: " + args[i]);
+//            }
+//        };
+//    }
 }
