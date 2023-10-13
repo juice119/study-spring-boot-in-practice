448f39a12f243a3dc62ac6b566bd010f7f82e5d4
Juice <3221ju@gmail.com>
2023-10-14 03:50:03 +0900

    [STUDY-15] 2.4 log4j2 추가

--- ch02/spring-boot-logging-with-log4j2/src/main/java/com/manning/sbip/ch01/SpringBootAppDemoApplication.java
+++ ch02/spring-boot-logging-with-log4j2/src/main/java/com/manning/sbip/ch01/SpringBootAppDemoApplication.java
@@ -1,13 +1,19 @@
 package com.manning.sbip.ch01;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootAppDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAppDemoApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(
+            SpringBootAppDemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAppDemoApplication.class, args);
+        logger.info("CourseTrackerApplication started successfully with Log4j2 configuration");
+    }
 
 }
