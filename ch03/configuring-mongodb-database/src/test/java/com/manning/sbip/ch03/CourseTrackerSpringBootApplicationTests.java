c9d19da0f6a86035e7dd1a5d1935041f7dea5064
Juice <3221ju@gmail.com>
2023-10-16 10:35:58 +0900

    [STUDY-16] 3.2.2 mongoDB 연동

--- ch03/configuring-mongodb-database/src/test/java/com/manning/sbip/ch03/CourseTrackerSpringBootApplicationTests.java
+++ ch03/configuring-mongodb-database/src/test/java/com/manning/sbip/ch03/CourseTrackerSpringBootApplicationTests.java
@@ -1,12 +1,31 @@
 package com.manning.sbip.ch03;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBObject;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootTest
 class CourseTrackerSpringBootApplicationTests {
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
     @Test
-    void contextLoads() {
+    void givenObjectAvailableWhenSaveToCollectionThenExpectValue() {
+        // given
+        DBObject dbObject = BasicDBObjectBuilder.start().add("Manning", "Spring Boot In Practice")
+            .get();
+
+        // when
+        mongoTemplate.save(dbObject, "collection");
+
+        // then
+        assertThat(mongoTemplate.findAll(DBObject.class, "collection"))
+            .extracting("Manning")
+            .containsOnly("Spring Boot In Practice");
     }
 }
