d9f4cf4a202e3b5e5f830049f7a78156b689c2b2
Juice <3221ju@gmail.com>
2023-10-16 13:36:14 +0900

    [STUDY-16] 페이지 네이션

--- ch03/using-paging-and-sorting-repository/src/test/java/com/manning/sbip/ch03/CourseTrackerSpringBootApplicationTests.java
+++ ch03/using-paging-and-sorting-repository/src/test/java/com/manning/sbip/ch03/CourseTrackerSpringBootApplicationTests.java
@@ -1,9 +1,56 @@
 package com.manning.sbip.ch03;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import com.manning.sbip.ch03.model.Course;
+import com.manning.sbip.ch03.repository.CourseRepository;
+import org.assertj.core.api.Condition;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@DataJpaTest
 class CourseTrackerSpringBootApplicationTests {
 
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Test
+    void 페이지는_5개의_레코드로_조회된다() {
+        Pageable pageable = PageRequest.of(0, 5);
+        assertThat(courseRepository.findAll(pageable)).hasSize(5);
+        assertThat(pageable.getPageNumber()).isEqualTo(0);
+
+        Pageable nextPageable = pageable.next();
+        assertThat(courseRepository.findAll(nextPageable)).hasSize(4);
+        assertThat(nextPageable.getPageNumber()).isEqualTo(1);
+    }
+
+
+    @Test
+    void givenDataAvailableWhenSortsFirstPageThenGetSortedSData() {
+        Pageable pageable = PageRequest.of(0,5, Sort.by(Sort.Order.asc("Name")));
+        Condition<Course> sortedFirstCourseCondition = new Condition<Course>() {
+            @Override
+            public boolean matches(Course course) {
+                return course.getId() == 4 && course.getName().equals("Cloud Native Spring Boot Application Development");
+            }
+        };
+        assertThat(courseRepository.findAll(pageable)).first().has(sortedFirstCourseCondition);
+    }
+
+    @Test
+    void givenDataAvailableWhenApplyCustomSortThenGetSortedResult() {
+        Pageable customSortPageable = PageRequest.of(0,5, Sort.by("Rating").descending().and(Sort.by("Name")));
+        Condition<Course> customSortFirstCourseCondition = new Condition<Course>() {
+            @Override
+            public boolean matches(Course course) {
+                return course.getId() == 2 && course.getName().equals("Getting Started with Spring Security DSL");
+            }
+        };
+        assertThat(courseRepository.findAll(customSortPageable)).first().has(customSortFirstCourseCondition);
+    }
 }
