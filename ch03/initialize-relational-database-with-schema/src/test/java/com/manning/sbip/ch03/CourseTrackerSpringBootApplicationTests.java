81fdc4be699c5c63f187b5d19647c293a227ee11
Juice <3221ju@gmail.com>
2023-10-16 11:24:09 +0900

    [STUDY-16] chapter 3.2.3 sql 파일로 스키마,데이터 초기화

--- ch03/initialize-relational-database-with-schema/src/test/java/com/manning/sbip/ch03/CourseTrackerSpringBootApplicationTests.java
+++ ch03/initialize-relational-database-with-schema/src/test/java/com/manning/sbip/ch03/CourseTrackerSpringBootApplicationTests.java
@@ -1,14 +1,37 @@
 package com.manning.sbip.ch03;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CourseTrackerSpringBootApplicationTests {
-
+    @Autowired
+    private DataSource dataSource;
 
     @Test
-    void contextLoads() {
-    }
+    void whenCountAllCoursesThenExpectFiveCourses() throws SQLException {
+        ResultSet rs = null;
+        int noOfCourses = 0;
 
+        try(PreparedStatement ps = dataSource.getConnection().prepareStatement("SELECT COUNT(1) FROM COURSES")) {
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                noOfCourses = rs.getInt(1);
+            }
+            assertThat(noOfCourses).isEqualTo(5L);
+        } finally {
+            if(rs != null) {
+                rs.close();
+            }
+        }
+
+    }
 }
