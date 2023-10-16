d847b39c443c30ebb94fe2e431864fc5220ca5ea
Juice <3221ju@gmail.com>
2023-10-16 10:17:46 +0900

    [STUDY-16] 3.2.1 관계형 데이터 베이스 연동

--- ch03/configuring-relational-database/src/test/java/com/manning/sbip/ch03/CourseTrackerSpringBootApplicationTests.java
+++ ch03/configuring-relational-database/src/test/java/com/manning/sbip/ch03/CourseTrackerSpringBootApplicationTests.java
@@ -1,12 +1,22 @@
 package com.manning.sbip.ch03;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CourseTrackerSpringBootApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
-    void contextLoads() {
+    void givenDatasourceAvailableWhenAccessDetailsThenExpectDetails() throws SQLException {
+        assertThat(dataSource.getClass().getName()).isEqualTo("com.zaxxer.hikari.HikariDataSource");
+        assertThat(dataSource.getConnection().getMetaData().getDatabaseProductName()).isEqualTo("H2");
     }
 }
