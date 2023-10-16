5a5ce697c455509bfb1e32f8ddf63f5ed2041c08
Juice <3221ju@gmail.com>
2023-10-16 12:49:09 +0900

    [STUDY-16] 3.3.2 Custom Repository 로 필요한 메소드만 노출

--- ch03/creating-custom-spring-data-repository/src/test/java/com/manning/sbip/ch03/CourseTrackerSpringBootApplicationTests.java
+++ ch03/creating-custom-spring-data-repository/src/test/java/com/manning/sbip/ch03/CourseTrackerSpringBootApplicationTests.java
@@ -1,12 +1,30 @@
 package com.manning.sbip.ch03;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.manning.sbip.ch03.model.Course;
+import com.manning.sbip.ch03.repository.CustomizedCourseRepository;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@DataJpaTest
 class CourseTrackerSpringBootApplicationTests {
 
+    @Autowired
+    private CustomizedCourseRepository courseRepository;
+
     @Test
-    void contextLoads() {
+    void 강의_생성_후_조회_된다() {
+        // given
+        Course course = new Course("Rapid Spring Boot Application Development", "Spring", 4, "'Spring Boot gives all the power of the Spring Framework without all of the complexities");
+        
+        // when
+        courseRepository.save(course);
+
+        // then
+        assertThat(Arrays.asList(courseRepository.findAll()).size()).isEqualTo(1);
     }
 }
