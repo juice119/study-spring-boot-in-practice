dc7a4acbcdd4ff2559ddeee08f7b3afa3b83a80a
Juice <3221ju@gmail.com>
2024-01-18 22:46:31 +0900

    [STUDY-16] QueryDSL 로 타입 안정성 및 fluentApi 를 통한 코드 간결화

--- ch03/using-query-dsl/src/main/java/com/manning/sbip/ch03/repository/CourseRepository.java
+++ ch03/using-query-dsl/src/main/java/com/manning/sbip/ch03/repository/CourseRepository.java
@@ -1,11 +1,11 @@
 package com.manning.sbip.ch03.repository;
 
 import com.manning.sbip.ch03.model.Course;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseRepository extends CrudRepository<Course, Long> {
+public interface CourseRepository extends CrudRepository<Course, Long>, QuerydslPredicateExecutor<Course> {
 
-   // Methods to be added
 }
